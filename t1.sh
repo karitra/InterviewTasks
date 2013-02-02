00fdc867311701f7b803d02502d3cf4d4830778e
karitra <karev.alexandre@gmail.com>
2013-02-02 17:15:17 +0400

    tests: more comprehensive tests

--- t1.sh
+++ t1.sh
@@ -16,6 +16,52 @@ for i in {2049..4096}; do
 done >> $tmp
 
 cat $tmp | ./t1 | tail -n 1
+> $tmp
+
+for i in {1..1024}; do
+	echo -n "$i "
+done >> $tmp
+
+for i in {1026..2047}; do
+	echo -n "$i "
+done >> $tmp
+
+for i in {2049..4097}; do
+	echo -n "$i "
+done >> $tmp
+
+cat $tmp | ./t1 | tail -n 1
+> $tmp
+
+
+for i in {1..1025}; do
+	echo -n "$i "
+done >> $tmp
+
+for i in {1027..2048}; do
+	echo -n "$i "
+done >> $tmp
+
+for i in {2050..4096}; do
+	echo -n "$i "
+done >> $tmp
+
+cat $tmp | ./t1 | tail -n 1
+> $tmp
+
+for i in {1..1025}; do
+	echo -n "$i "
+done >> $tmp
+
+for i in {1027..2048}; do
+	echo -n "$i "
+done >> $tmp
+
+for i in {2050..4097}; do
+	echo -n "$i "
+done >> $tmp
+
+cat $tmp | ./t1 | tail -n 1
 
 rm $tmp
 
